6e7639c38b7a9d55145b59ae08ea10fa6964bad8
cBoden <62156116+Hell0w0@users.noreply.github.com>
2022-02-09 16:50:31 +0100

    Updated html with thymeleaf

--- src/main/java/se/kth/iv1201/project/presentation/LoginForm.java
+++ src/main/java/se/kth/iv1201/project/presentation/LoginForm.java
@@ -4,34 +4,16 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
-
-class CreateUserForm {
+ class LoginForm {
 
     @NotBlank(message = "Please specify user name")
 
     @Pattern(regexp = "^[\\p{L}\\p{M}*]*$", message = "Only letters are allowed")
     @Size(min = 2, max = 30, message = "Name must have min 2 and max 30 characters")
- 
-    private String firstName;
-    private String lastName;
-    private String  personNr;
+
     private String  email;
     private String password;
     private String username;
-
-    /**
-     * @return The initial FirstName of the account that will be created.
-     */
-    public String getFirstName() {
-        return firstName;
-    }
-
-    /**
-     * @param FirstName The initial FirstName of the account that will be created.
-     */
-    public void setFirstName(String newElem) {
-        firstName = newElem;
-    }
 
     /**
      * @return The name of the holderName of the account that will be created.
@@ -75,32 +57,5 @@ class CreateUserForm {
     public void setEmail(String newElem) {
         email = newElem;
     }
-        /**
-     * @return The name of the holderName of the account that will be created.
-     */
-    public String getPersonNr() {
-        return personNr;
-    }
 
-    /**
-     * @param holderName The name of the holderName of the account that will be
-     *               created.
-     */
-    public void setPersonNr(String newElem) {
-        personNr = newElem;
-    }
-    /**
-     * @return The name of the holderName of the account that will be created.
-     */
-    public String getLastName() {
-        return lastName;
-    }
-
-    /**
-     * @param holderName The name of the holderName of the account that will be
-     *               created.
-     */
-    public void setLastName(String newName) {
-        lastName = newName;
-    }
 }
--- src/main/java/se/kth/iv1201/project/presentation/UserController.java
+++ src/main/java/se/kth/iv1201/project/presentation/UserController.java
@@ -3,8 +3,13 @@ package se.kth.iv1201.project.presentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import se.kth.iv1201.project.application.UserService;
+import se.kth.iv1201.project.domain.IllegalUserRegistrationException;
+import se.kth.iv1201.project.domain.UserDTO;
 
 
 @Controller
@@ -12,23 +17,38 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
         static final String DEFAULT_PAGE_URL = "/";
         static final String HOME_PAGE_URL = "/application";
+        @Autowired
+        private UserService service;
+        private UserDTO currentUser;
 
-    @Autowired
     /**
      * No page is specified, redirect to the login page.
      *
      * @return A response that redirects the browser to the welcome page.
      */
     @GetMapping("/")
-    public String login() {
+    public String login(LoginForm loginForm) {
         return  "login";
     }
+
+    @PostMapping("/") 
+    public String application(LoginForm loginForm, Model model){
+        currentUser = service.CheckUserAndRole(loginForm.getEmail(),loginForm.getPassword());
+        model.addAttribute("currentUser", currentUser);
+        return "application";
+    }
     @GetMapping("/signup")
-    public String signup() {
+    public String signup(CreateUserForm createUserForm) {
         return  "signup";
     }
-    @PostMapping("/application")
-    public String application() {
-        return  "application";
+
+    @PostMapping("/signup") // When createUserAndCheckRole is finished change the type of currentUser to UserDTO. 
+    public String application(CreateUserForm createUserForm, Model model) throws IllegalUserRegistrationException{
+        String roleName = "Applicant";
+        currentUser = service.createUser(createUserForm.getFirstName(),createUserForm.getLastName(),createUserForm.getPersonNr(),createUserForm.getEmail(),createUserForm.getPassword(),roleName,createUserForm.getUsername());
+            model.addAttribute("currentUser", currentUser);
+        System.out.println(currentUser.getFirstName());
+        return "application";
     }
+
 }
