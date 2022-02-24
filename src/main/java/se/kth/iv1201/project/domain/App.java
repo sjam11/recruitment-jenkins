d07feed1fe1529a5f4bde72236f19eb97979a943
cBoden <62156116+Hell0w0@users.noreply.github.com>
2022-02-24 15:26:27 +0100

    Added App function
    Used to get the users full application

---
+++ src/main/java/se/kth/iv1201/project/domain/App.java
@@ -0,0 +1,39 @@
+package se.kth.iv1201.project.domain;
+
+import java.util.HashMap;
+
+public class App {
+
+    private User person;
+    private HashMap<String, String> availability;
+    private HashMap<String, Integer> competence;
+
+    public App(User person, HashMap<String,Integer> competence, HashMap<String,String> availability){
+        this.person = person;
+        this.competence = competence;
+        this.availability = availability;
+    }
+    public void setAvailability(HashMap<String,String> availability){
+        this.availability = availability;
+    }
+
+    public void setPerson(User person){
+        this.person = person;
+    }
+
+    public void setCompence(HashMap<String,Integer> competence){
+        this.competence = competence;
+    }
+
+    public HashMap<String,Integer> getCompetence(){
+        return competence;
+    }
+    public HashMap<String,String> getAvailability(){
+        return availability;
+    }
+    public UserDTO getPerson(){
+        return person;
+    }
+
+
+}
---
+++ src/main/java/se/kth/iv1201/project/presentation/CompetenceForm.java
@@ -0,0 +1,39 @@
+package se.kth.iv1201.project.presentation;
+
+import java.math.BigDecimal;
+
+class CompetenceForm {
+
+    private String expertise;
+    private BigDecimal years;
+
+    /** 
+     * @return String the area of expertise the user has
+     */
+    public String getExpertise() {
+        return expertise;
+    }
+
+    
+    /** 
+     * @param newElem The new area of expertise of the user 
+    */
+    public void setExpertise(String newElem) {
+        expertise= newElem;
+    }
+
+    /** 
+     * @return BigDecimal the amount of years in the area of expertise the user has
+     */
+    public BigDecimal getYears() {
+        return years;
+    }
+
+    
+    /** 
+     * @param newElem The amount of years in the area of expertise the user has
+    */
+    public void setYears(BigDecimal newElem) {
+        years = newElem;
+    }
+}
